329b063de257b9c4ade2744f7b9e8da007f9872f
Muhammad Syamfiraas Akbar <syamfiraas48@gmail.com>
2023-09-10 21:36:55 +0700

    Vid 4

---
+++ js/operator.js
@@ -0,0 +1,12 @@
+let x = "ma";
+let y = "ja";
+
+let u = x + y;
+
+console.log(u);
+
+let e;
+e = 8**9;
+console.log(e);
+e = e**0;
+console.log(e);
---
+++ js/tipedata.js
@@ -0,0 +1,5 @@
+let kata = "anjay";
+let bool = false;
+let kosong = null;
+
+console.log(`${kata} ${bool}`);
